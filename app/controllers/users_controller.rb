d2fdd54d22884f3f2a1b70e527da3dc8aedca964
Keisuke Yoshida <kskysd0706@gmail.com>
2022-04-06 08:56:05 +0000

    [Finish]guest_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   before_action :ensure_correct_user, only: [:edit,:update]
-  def ensure_correct_user
-   if @current_user != params[:id].to_i
-     redirect_to user_path(current_user)
-   end
-  end
 
   def show
     @user = User.find(params[:id])
@@ -43,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
 
 end
 
